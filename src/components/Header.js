c30812a633b3b801d9df2631431649b1d9e61121
Aknandan Sharma <90132050+aknandansharma@users.noreply.github.com>
2022-12-05 22:13:39 +0530

    final commit 1

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+export default function Header() {
+  return (
+    <div className="logodiv">
+      <img
+        src="https://png.pngitem.com/pimgs/s/520-5208614_curso-programacin-front-end-completo-transparent-html-css.png"
+        alt="logo"
+      />
+      <h2 className="logo-text">Code Editor</h2>
+      <p className="logo-text-name">By aknandan kumar sharma</p>
+      <nav>
+       
+      </nav>
+    </div>
+  );
+}
